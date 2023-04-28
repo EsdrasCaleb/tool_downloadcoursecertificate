fa17294d80f0cb6d2c131b2db88707fdb0983c5b
Esdras Caleb <esdrascaleb@gmail.com>
2023-04-28 17:02:12 -0300

    initial commit

---
+++ lang/en/tool_downloadcoursecertificate.php
@@ -0,0 +1,5 @@
+<?php
+$string['pluginname'] = 'Download Moodle Workplace Certificate';
+$string['courses'] = "Course(s):";
+$string['users'] = "User(s):";
+$string['certificates'] = "Certificate(s):";
